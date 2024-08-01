5b93efeea7d61066a9cce06823b86de58214af90
hetbhalani <bhalanihet2006@gmail.com>
2024-08-01 22:07:18 +0530

    update

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -2,10 +2,18 @@
 import List from "./List";
 
 function App() {
-  return(
+    const fruits = [{id: 1, name: "apple", cals: 80},
+      {id: 2, name: "banana", cals: 90},
+      {id: 3, name: "orange", cals: 130},
+      {id: 4, name: "watermelon", cals: 70},
+      {id: 5, name: "grapes", cals: 110},
+  ];
+
+  return(  
     <>
         {/* <UserGreeting isLogedin={true} userName="Het Bhalani"></UserGreeting> */}
-        <List></List>
+        
+        <List items = {fruits} category = "Fruits"></List>
     </>
   );
 }
--- React/my-react-app/src/List.jsx
+++ React/my-react-app/src/List.jsx
@@ -1,25 +1,31 @@
 
-function List(){
+function List(props){
     // const fruits = ["apple" , "banana", "orange", "watermelon", "grapes"]
     // fruits.sort();
 
-    const fruits = [{id: 1, name: "apple", cals: 80},
-                    {id: 2, name: "banana", cals: 90},
-                    {id: 3, name: "orange", cals: 130},
-                    {id: 4, name: "watermelon", cals: 70},
-                    {id: 5, name: "grapes", cals: 110},
-    ]
+    // const fruits = [{id: 1, name: "apple", cals: 80},
+    //                 {id: 2, name: "banana", cals: 90},
+    //                 {id: 3, name: "orange", cals: 130},
+    //                 {id: 4, name: "watermelon", cals: 70},
+    //                 {id: 5, name: "grapes", cals: 110},
+    // ]
     // fruits.sort((a,b) => a.name.localeCompare(b.name)) //sort kre object na names ne
     // fruits.sort((a,b) => b.name.localeCompare(a.name)) //undhu sort krva mate
     // fruits.sort((a,b) => b.cals - a.cals) //numeric ne reverse sort kre
     // fruits.sort((a,b) => a.cals - b.cals) // numeric ne sort kre
 
     // const lowCalFruits = fruits.filter(fruits => fruits.cals < 100);
+    const category = props.category;
+    const itemList = props.items;   
 
-    const ListItems = lowCalFruits.map(lowCalFruit => <li key = {lowCalFruit.id}> {lowCalFruit.name}: &nbsp; 
-    <b>{lowCalFruit.cals}</b></li>);
+    const ListItems = itemList.map(item => <li key = {item.id}> {item.name}: &nbsp; 
+    <b>{item.cals}</b></li>);
 
-    return(<ol>{ListItems}</ol>);
+    return(<>
+        <h3>{category}</h3>
+        <ol>{ListItems}</ol>
+    </>
+    );
 }
 
 export default List
