d9ee888a564970c00def9ceaa225e0f68284fed1
hetbhalani <bhalanihet2006@gmail.com>
2024-08-01 21:33:11 +0530

    update

--- React/my-react-app/src/List.jsx
+++ React/my-react-app/src/List.jsx
@@ -3,14 +3,21 @@ function List(){
     // const fruits = ["apple" , "banana", "orange", "watermelon", "grapes"]
     // fruits.sort();
 
-    const fruits = [{name: "apple", cals: 80},
-                    {name: "banana", cals: 90},
-                    {name: "orange", cals: 100},
-                    {name: "watermelon", cals: 110},
-                    {name: "grapes", cals: 120},
+    const fruits = [{id: 1, name: "apple", cals: 80},
+                    {id: 2, name: "banana", cals: 90},
+                    {id: 3, name: "orange", cals: 130},
+                    {id: 4, name: "watermelon", cals: 70},
+                    {id: 5, name: "grapes", cals: 110},
     ]
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)) //sort kre object na names ne
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)) //undhu sort krva mate
+    // fruits.sort((a,b) => b.cals - a.cals) //numeric ne reverse sort kre
+    // fruits.sort((a,b) => a.cals - b.cals) // numeric ne sort kre
 
-    const ListItems = fruits.map(fruits => <li key = {fruits.name}> {fruits.name}</li>)
+    // const lowCalFruits = fruits.filter(fruits => fruits.cals < 100);
+
+    const ListItems = lowCalFruits.map(lowCalFruit => <li key = {lowCalFruit.id}> {lowCalFruit.name}: &nbsp; 
+    <b>{lowCalFruit.cals}</b></li>);
 
     return(<ol>{ListItems}</ol>);
 }
