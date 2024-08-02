a7f2a6d062bfcff043c48f0de727a7c7dce9a4c5
hetbhalani <bhalanihet2006@gmail.com>
2024-08-03 00:12:31 +0530

    some examples

--- Pyhton/Constructor.py
+++ Pyhton/Constructor.py
@@ -1,23 +1,23 @@
-class het:
-    name = "Het"
-    langauge= "Python"
-    sem = 3
+# class het:
+#     name = "Het"
+#     langauge= "Python"
+#     sem = 3
 
-    def __init__(self):
-        print("This is an object")
+#     def __init__(self):
+#         print("This is an object")
 
-    def info(self):
-        print(f"name is {self.name} and langauge known is {self.langauge}")
+#     def info(self):
+#         print(f"name is {self.name} and langauge known is {self.langauge}")
 
-    @staticmethod
-    def greet():
-        print("Good morning")
+#     @staticmethod
+#     def greet():
+#         print("Good morning")
 
-obj = het()
-obj.langauge = "Javascript"
-print(obj.name, obj.langauge)
+# obj = het()
+# obj.langauge = "Javascript"
+# print(obj.name, obj.langauge)
 
-
+#######################################################
 
 class program:
     compeny = "Microsoft"
@@ -27,4 +27,26 @@ class program:
          self.langauge = langauge
          self.salary = salary
 
-objject = program("Het", )
+p = program("Het", "JavaScript", 1234567890)
+print(p.name, p.langauge, p.salary)
+
+#################################################################
+
+class calc:
+    def __init__(self, n):
+        self.n = n
+    
+    def squre(self):
+     print(f"Squre is {self.n * self.n}")
+
+    def squreRoot(self):
+     print(f"squreRoot is {self.n ** 1/2}")
+
+    def cube(self):
+     print(f"cube is {self.n * self.n * self.n}")
+
+a = calc(4)
+a.squre()
+a.squreRoot()
+a.cube()
+
