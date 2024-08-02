f84e30794c907f6bad75efe60624bf88214bdd55
hetbhalani <bhalanihet2006@gmail.com>
2024-08-02 23:45:43 +0530

    classes

--- Pyhton/Class.py
+++ Pyhton/Class.py
@@ -3,5 +3,14 @@ class het:
     langauge= "Python"
     sem = 3
 
+    def info(self):
+        print(f"name is {self.name} and langauge known is {self.langauge}")
+
+    @staticmethod
+    def greet():
+        print("Good morning")
+
 obj = het()
-print(het.name, het.langauge, het.sem)
+obj.langauge = "Javascript"
+obj.info()
+obj.greet()
