c8f25ee80dbb916f6519a9560ba1c0948cdcac8c
hetbhalani <bhalanihet2006@gmail.com>
2024-08-05 08:54:24 +0530

    update

--- Pyhton/Decorators.py
+++ Pyhton/Decorators.py
@@ -1,12 +1,17 @@
 #class method
 
 class Employee:
-    a = 1
-    @classmethod
-    def show(cls):
-        print(cls.a)
+    # a = 1
+    # @a.setter
+    # def a(self,val):
+
+    @property
+    def a(self):
+        return self.obj
+    # @classmethod
+    # def show(cls):
+    #     print(cls.a)
 
 obj = Employee()
-obj.a = 123
-
-obj.show()
+obj.a = 10
+print(obj.a)
--- Pyhton/GuessTheNumber.py
+++ Pyhton/GuessTheNumber.py
@@ -7,12 +7,13 @@ a = -1
 while(a != n):
     cnt+=1
     a = int(input("Guess the number: "))
-
+        
     if(a > n):
         print("Lower")
         
     elif(a < n):
         print("Higher")
+
 
 print(f"Yesss!!! the number was {n}") 
 print(f"You guessed number in {cnt} guesses")
