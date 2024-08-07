7bf6482d5b2728347de9db079d60e1d7aed13e36
hetbhalani <bhalanihet2006@gmail.com>
2024-08-07 14:50:02 +0530

    new

--- Pyhton/AdvancedPython.py
+++ Pyhton/AdvancedPython.py
@@ -11,3 +11,20 @@ def greet(name: str) -> str:
     return f"Hi {name}"
 
 print(greet("Het"))
+
+#lambda
+
+mult = lambda x,y: x*y
+
+print(mult(2,4))
+
+#enumarator
+l = [1,3,6,2,4,9]
+
+# index = 0
+# for item in l:
+#     print(f"{item} at index {index}")
+#     index+=1
+
+for index,item in enumerate(l):
+    print(f"{item} at index {index}")
