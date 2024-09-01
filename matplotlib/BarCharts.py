c2cf695c45500bb6f6e8dff6c1b36390a848806f
hetbhalani <bhalanihet2006@gmail.com>
2024-09-02 00:24:20 +0530

    update graphs

--- matplotlib/BarCharts.py
+++ matplotlib/BarCharts.py
@@ -1,6 +1,4 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
 labels = ['A','B','C']
 values = [3,6,9]
--- matplotlib/graph.py
+++ matplotlib/graph.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 x = [0,1,2,3,4]
 y = [0,2,4,6,8]
