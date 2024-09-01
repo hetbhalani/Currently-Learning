2b392893b9caba40163bdff127d0c208e15fd32e
hetbhalani <bhalanihet2006@gmail.com>
2024-09-01 23:59:28 +0530

    learn graph

--- matplotlib/BarCharts.py
+++ matplotlib/BarCharts.py
@@ -5,6 +5,9 @@ import pandas as pd
 labels = ['A','B','C']
 values = [3,6,9]
 
-plt.bar(labels,values)
+bars = plt.bar(labels,values)
+bars[0].set_hatch('/')
+bars[1].set_hatch('o')
+bars[2].set_hatch('*')
 
 plt.show()
