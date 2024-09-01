dcb28de87d1756b107a04ff6399ac4d84052128f
hetbhalani <bhalanihet2006@gmail.com>
2024-09-01 11:48:16 +0530

    learn graph

--- matplotlib/graph.py
+++ matplotlib/graph.py
@@ -7,4 +7,13 @@ y = [0,2,4,6,8]
 
 plt.plot(x,y, label='2x', color='red', linewidth=2, marker='.', linestyle='--', markersize=10, markeredgecolor='blue')
 
+plt.title('Our First Graph!', fontdict={'fontname': 'Comic Sans MS', 'fontsize': 20})
+
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+plt.xticks([0,1,2,3,4,])
+plt.yticks([0,2,4,6,8,10])
+
+plt.legend()
 plt.show()
