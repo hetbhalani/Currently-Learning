a64c8df58c126f81ed63055925f7d5101a429149
hetbhalani <bhalanihet2006@gmail.com>
2024-09-01 23:38:29 +0530

    learn graph

--- matplotlib/graph.py
+++ matplotlib/graph.py
@@ -7,6 +7,9 @@ y = [0,2,4,6,8]
 
 plt.plot(x,y, label='2x', color='red', linewidth=2, linestyle='--', marker='.', markersize=10)
 
+x2 = np.arange(0,4.5,.5)
+plt.plot(x2, x2**2)
+
 plt.title('Our First Graph!', fontdict={'fontname': 'Comic Sans MS', 'fontsize': 20})
 
 plt.xlabel('X Axis')
