907186f31760c3384d339799d449d553310c7ad6
hetbhalani <bhalanihet2006@gmail.com>
2024-09-06 01:36:27 +0530

    learn react

--- React/daily/src/index.js
+++ React/daily/src/index.js
@@ -1,33 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
 const data = [
   {
     name: "Het",
     img: "https://picsum.photos/300/200?random=1",
-    url: "https://www.linkedin.com/in/het-bhalani-20403b2a8/"
+    url: "https://www.linkedin.com/in/het-bhalani-20403b2a8/",
+    about: "Machine learning Engineer at ABC Corp."
   },
   {
     name: "Dhruv",
     img: "https://picsum.photos/300/200?random=2",
-    url:"https://www.linkedin.com/in/dhruv-chotai-179a502aa/"
+    url:"https://www.linkedin.com/in/dhruv-chotai-179a502aa/",
+    about: "Data Scientist at XYZ Ltd."
   },
   {
     name: "Meet",
     img: "https://picsum.photos/300/200?random=3",
-    url:"https://linkedin.com/in/meet-padhiar-03344a2b4/"
+    url:"https://linkedin.com/in/meet-padhiar-03344a2b4/",
+    about: "Web Developer specializing in React."
   },
   {
     name: "Mann",
     img: "https://picsum.photos/300/200?random=4",
-    url:"https://www.linkedin.com/in/mann-padhiar-9139972b3/"
+    url:"https://www.linkedin.com/in/mann-padhiar-9139972b3/",
+    about: "Full Stack Developer at Tech Solutions."
   },
   {
     name: "Vandan",
     img: "https://picsum.photos/300/200?random=5",
-    url:"https://www.linkedin.com/in/vandan-kholia-a63b82294/"
+    url:"https://www.linkedin.com/in/vandan-kholia-a63b82294/",
+    about: "Mobile App Developer at Innovate Inc."
   },
 ];
 
@@ -40,8 +44,7 @@ const newarr = data.map((dude) => {
           <div className="card-body">
             <h5 className="card-title">{dude.name}</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              {dude.about}
             </p>
             <button className="btn btn-primary"  onClick = {()=>window.open(dude.url, "_blank")}>
              
