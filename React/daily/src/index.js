5dd1a66e66ed83fb8cc0b9a707d87ab125014e9d
hetbhalani <bhalanihet2006@gmail.com>
2024-09-06 01:17:12 +0530

    learn react

---
+++ React/daily/src/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const data = [
+  {
+    name: "Het",
+    img: "https://picsum.photos/300/200?random=1",
+  },
+  {
+    name: "Dhruv",
+    img: "https://picsum.photos/300/200?random=2",
+  },
+  {
+    name: "Meet",
+    img: "https://picsum.photos/300/200?random=3",
+  },
+  {
+    name: "Mann",
+    img: "https://picsum.photos/300/200?random=4",
+  },
+  {
+    name: "Vandan",
+    img: "https://picsum.photos/300/200?random=5",
+  },
+];
+
+const newarr = data.map((dude) => {
+  return (
+    
+      <div className="col col-4">
+        <div className="card my-3">
+          <img src={dude.img} className="card-img-top" alt={dude.name} />
+          <div className="card-body">
+            <h5 className="card-title">{dude.name}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+  );
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<>
+  <div className="container-sm m-4">
+    <div className="row">{newarr}</div>
+  </div>
+</>
+);
