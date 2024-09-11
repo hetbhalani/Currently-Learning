4de351bc47db1cccbc67bf2698d28784aeaf4d14
hetbhalani <bhalanihet2006@gmail.com>
2024-09-11 20:00:57 +0530

    insertion

---
+++ PracticeSorting/InsertionSort.py
@@ -0,0 +1,13 @@
+a = [2,5,3,7,4,1,9]
+n = len(a)
+
+for i in range (n):
+    key = a[i]
+    j = i-1
+    
+    while(j >= 0 and a[j] > key):
+        a[j+1] = a[j]
+        j-=1
+    a[j+1] = key
+    
+print(a)
