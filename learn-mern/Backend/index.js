a0cffb51b4a63af1a3b03cf8c815783fc92bf1ed
hetbhalani <bhalanihet2006@gmail.com>
2024-09-17 18:14:02 +0530

    Database Done

--- learn-mern/Backend/index.js
+++ learn-mern/Backend/index.js
@@ -1,14 +1,44 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import { Book } from './models/booksModel.js';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req,res)=>{
     console.log(req);
     return res.status(200).send("Welcome Dostoooo!")
+})
+
+
+app.post('/books', async(req,res)=>{
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        )
+        {
+            return res.status(400).send({message:'Fill all the details'});
+        }
+
+        const newBook = {
+           title:  req.body.title, 
+           author: req.body.author,
+           publishYear: req.body.publishYear
+        }
+
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message:error.message});
+    }
 })
 
 mongoose.connect(process.env.MONGO_URL)
