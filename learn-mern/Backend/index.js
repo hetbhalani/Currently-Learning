625ed35939c01a38f531b92ab55e9b3f309b68d8
hetbhalani <bhalanihet2006@gmail.com>
2024-09-17 16:41:05 +0530

    add database

--- learn-mern/Backend/index.js
+++ learn-mern/Backend/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, ()=>{
-    console.log("server started!");
+app.get('/', (req,res)=>{
+    console.log(req);
+    return res.status(200).send("Welcome Dostoooo!")
 })
 
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("Connected to Database");
+    app.listen(port, ()=>{
+        console.log("server started!");
+    })
+})
+.catch((err)=>{
+    console.log(err);
+})
