eb2dc27ed8f8ca86367326b9553ecb24a3247c88
hetbhalani <bhalanihet2006@gmail.com>
2024-09-18 22:19:49 +0530

    home page

--- learn-mern/Frontend/src/Pages/Home.jsx
+++ learn-mern/Frontend/src/Pages/Home.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import {Link} from 'react-router-dom';
+import {AiOutlineEdit} from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
 
 function Home() {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true);
+        axios.get('http://localhost:3690/books').then((res)=>{
+            setBooks(res.data.data);
+            setLoading(false);
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoading(false);
+          });
+    },[]);
   return (
-    <div>Home</div>
+    <div className='p-4'>
+        <div className='flex justify-between items-center'>
+            <h1 className='text-3xl my-8'>Books List</h1>
+            <Link to='/books/create'>
+            <MdOutlineAddBox className='text-sky-800 text-4xl' />
+            </Link>
+        </div>
+        {loading?(<Spinner/>):(
+            <table className='w-full border-separate boarder-spacing-2'>
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        )}
+    </div>
   )
 }
 
