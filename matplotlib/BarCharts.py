878c95833d5c3850f63af70f02827df06f8f9e3b
hetbhalani <bhalanihet2006@gmail.com>
2024-10-03 22:46:56 +0530

    new graph

--- matplotlib/BarCharts.py
+++ matplotlib/BarCharts.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 
-labels = ['A','B','C']
-values = [3,6,9]
+labels = ['A','B','C','D','E']
+values = [3,6,9,12,15]
 
 bars = plt.bar(labels,values)
 bars[0].set_hatch('/')
 bars[1].set_hatch('o')
 bars[2].set_hatch('*')
+bars[3].set_hatch('o')
+bars[4].set_hatch('/')
 
 plt.show()
