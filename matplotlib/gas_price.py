2270ffeab10f7febed27e5956db4cd68c7f77f8a
hetbhalani <bhalanihet2006@gmail.com>
2024-10-04 23:42:35 +0530

    gas price graoh

--- matplotlib/gas_price.py
+++ matplotlib/gas_price.py
@@ -6,3 +6,9 @@ gas = pd.read_csv('gas_proces.csv')
 plt.figure(figsize=(8,5))
 
 plt.title('Gas Prices over Time (in USD)', fontdict={'fontweight':'bold', 'fontsize': 18})
+
+plt.plot(gas.Year, gas.USA,'b.-', lable='USA')
+plt.plot(gas.Year, gas.Canada, 'r.-', lable='Canada')
+plt.plot(gas.Year, gas['South Korea'], 'g.-')
+plt.plot(gas.Year, gas.Australia, 'y.-')
+
