8fbbb64b5d2be292b12085252b99f501b22a515c
hetbhalani <bhalanihet2006@gmail.com>
2024-10-19 00:15:50 +0530

    learn numpy

--- NumPy/intro.py
+++ NumPy/intro.py
@@ -8,3 +8,13 @@ list2 = ['Het :)', 18, list1, True]
 
 np1 = np.array([1,2,3,4,5,6,7])
 print(np1)
+print(np1.shape)
+
+np2 = np.arange(10)
+print(np2)
+
+np3 = np.arange(0,10,2) #from,to,step
+print(np3)
+
+np4 = np.zeros((5,10)) #dimention,no. of zeros 
+print(np4)
