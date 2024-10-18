6b032cc6c30bd7aa84e5c36c8eb1c00dd59f1227
hetbhalani <bhalanihet2006@gmail.com>
2024-10-19 00:38:23 +0530

    learn numpy

--- NumPy/slice.py
+++ NumPy/slice.py
@@ -10,3 +10,8 @@ print(np1[4:]) #4->end
 print(np1[-3:-1]) #start from back
 
 print(np1[1:5:2]) # , ,steps
+
+print(np1[::2]) #skip one
+
+np2 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(np2[1,2]) #8
