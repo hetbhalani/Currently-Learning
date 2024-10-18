5b94e0b40b62fc95a44667da3cd3ec57ae64c379
hetbhalani <bhalanihet2006@gmail.com>
2024-10-19 00:10:44 +0530

    learn numpy

--- NumPy/intro.py
+++ NumPy/intro.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 list1 = [1,2,3,4,5]
-print(list1)
+# print(list1)
 
 list2 = ['Het :)', 18, list1, True]
-print(list2)
+# print(list2)
+
+np1 = np.array([1,2,3,4,5,6,7])
+print(np1)
