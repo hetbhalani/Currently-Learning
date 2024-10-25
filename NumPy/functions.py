b293348008822fc125b22144d7d4fc53586d6994
hetbhalani <bhalanihet2006@gmail.com>
2024-10-25 22:11:45 +0530

    learn numpy

--- NumPy/functions.py
+++ NumPy/functions.py
@@ -5,3 +5,10 @@ np1 = np.array([0,-1,-2,3,4,5,6,7,8,9])
 print(np.sqrt(np1)) #it return nan at neg. numbers
 print(np.absolute(np1))
 
+print(np.exp(np1))
+
+print(np.min(np1))
+print(np.max(np1))
+
+print(np.sign(np1)) # -1,0,1
+
