b7f0a2a5cad714d2b3232506b3480244b0e457a2
hetbhalani <bhalanihet2006@gmail.com>
2024-12-11 18:59:53 +0530

    Learn oops

--- OOPs/class.py
+++ OOPs/class.py
@@ -7,10 +7,10 @@ item1.name = "Phone"
 item1.price = 100
 item1.quintity = 5
 
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quintity = 2
+# item2 = Item()
+# item2.name = "Laptop"
+# item2.price = 1000
+# item2.quintity = 2
 
 print(item1.calculate_total_price(item1.price,item1.quintity))
 
---
+++ OOPs/init.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+        print(f"Hi I'm {name}")
+        print(age)
+        
+        
+    
+s1 = Student("Het",18)
+# s1.name = "Het"
+# s1.age = 18
+
+s2 = Student("Bhai",12345)
+# s2.name = "Bhai"
+# s2.age = 1234
