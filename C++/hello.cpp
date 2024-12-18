87b5c159480918ae4eb9d78ff890b33bd2528f18
hetbhalani <bhalanihet2006@gmail.com>
2024-12-18 23:54:52 +0530

    learning

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+using namespace std;
 
 int main(){
-    std::cout << "Hello, world!" << std::endl;
+    string name = "Het";
+    int age = 18;
+    cout<<"My name is " << name <<" and my age is "<< age << endl;
     return 0;
 }
