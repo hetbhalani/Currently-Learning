7acea81deda7783b74280e8244b936cecafc38ac
hetbhalani <bhalanihet2006@gmail.com>
2024-12-18 21:26:55 +0530

    validation

--- OOPs/init.py
+++ OOPs/init.py
@@ -1,5 +1,9 @@
 class Student:
-    def __init__(self,name,age):
+    def __init__(self,name: str ,age: int):
+        #Validation
+        assert age >= 18, "you are underaged"
+        
+        
         self.name = name
         self.age = age
         
@@ -14,7 +18,7 @@ s1 = Student("Het",18)
 # s1.name = "Het"
 # s1.age = 18
 
-s2 = Student("Bhai",12345)
+s2 = Student("Bhai",12)
 # s2.name = "Bhai"
 # s2.age = 1234
 
