071ad22d0b2ed9e31916a202e9b89b3adfcc07d8
hetbhalani <bhalanihet2006@gmail.com>
2024-12-19 19:05:27 +0530

    __repr__

--- OOPs/init.py
+++ OOPs/init.py
@@ -1,5 +1,8 @@
 class Student:
     dis_fees = 0.8 #20% discount on fees 
+    
+    all = []
+    
     def __init__(self,name: str ,age: int, fees: int):
         #Validation
         assert age >= 18, "you are underaged"
@@ -12,17 +15,22 @@ class Student:
         # print(f'i am {age} years old')
         # print(f"i paid {fees} rupees")
         
+        #actions to execute
+        Student.all.append(self)
     def dblAge(self):
         return self.age*2
         
     def applyDis(self):
         self.fees = self.fees * Student.dis_fees
     
+    def __repr__(self): #represent the instance
+        return f"Student{self.fees}"
+    
 s1 = Student("Het",19,120000)
 # s1.name = "Het"
 # s1.age = 18
 
-s2 = Student("Bhai",20,50000)
+s2 = Student("Meet",20,50000)
 # s2.name = "Bhai"
 # s2.age = 1234
 
@@ -31,10 +39,17 @@ s2 = Student("Bhai",20,50000)
 # print(Student.__dict__) #print all class level attribute
 # print(s1.__dict__) #print all instence level attribute
 s1.applyDis()
-s2.applyDis()
 print(s1.fees)
-print(s2.fees)
 
 s2.dis_fees = 0.7
 s2.applyDis()
 print(s2.fees)
+
+s3 = Student("Mann",21,12344)
+s4 = Student("Dhruv",19,123456789)
+s5 = Student("Vandan", 20, 98765)
+
+for i in Student.all:
+    print(i.name)
+    
+print(Student.all) #__repr__ method for fees
