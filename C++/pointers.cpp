7b811d63259e114629c25289d009b351a97c4fbe
hetbhalani <bhalanihet2006@gmail.com>
2024-12-20 17:03:07 +0530

    pointers

---
+++ C++/pointers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 10;
+    int *Page = &age;
+    double cgpa = 9.9;
+    string name = "Het";
+
+    cout<< Page;
+    return 0;
+}
